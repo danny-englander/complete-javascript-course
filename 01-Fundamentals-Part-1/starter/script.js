213856f831343bff7c1e74f537cb19da93afab49
danny-englander <dannyenglander311@gmail.com>
2022-08-19 06:46:49 -0400

    Assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,21 @@
-// let js = 'amazing';
-// if (js === 'amazing') alert('js is fun')
+let js = "amazing";
+// Values, smallest unit of information in JS
+console.log("Jonas");
+console.log(23);
+console.log(50 + 4 + 6);
+let firstName = "Matilda";
+let $functon = "foo";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let person = "foo";
+let PI = 3.1415927;
+
+// Assignment
+let country = "Netherlands";
+let continent = "Europe";
+let population = 3044645450;
+let pop_str = population.toLocaleString("en-US");
+console.log(country);
+console.log(continent);
+console.log(pop_str);
