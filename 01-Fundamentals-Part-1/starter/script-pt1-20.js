8bc1ee1b14ea89bf072732064076f9dfc6ce39b6
danny-englander <dannyenglander311@gmail.com>
2022-08-19 15:22:52 -0400

    starting lesson 20, part 1

---
+++ 01-Fundamentals-Part-1/starter/script-pt1-20.js
@@ -0,0 +1,15 @@
+// Type conversion
+const inputYear = "1991";
+console.log(inputYear);
+// Convert string to number using Number
+strToNum = Number(inputYear);
+console.log(strToNum + 18);
+// This will print NaN as it cannot convert letters to a number
+console.log(Number("Jonas"));
+// This prints number but it is invalid.
+console.log(typeof NaN);
+// Convert number to string.
+console.log(String(23));
+
+// Type coercion
+console.log();
