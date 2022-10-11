a675fc20453793cee2963421fd72d602dd787231
danny-englander <dannyenglander311@gmail.com>
2022-10-11 15:32:45 -0400

    sect2-challenge-3

--- 02-Fundamentals-Part-2/starter/sect2-challenge-3.js
+++ 02-Fundamentals-Part-2/starter/sect2-challenge-3.js
@@ -2,9 +2,8 @@
 
 ///////////////////////////////////////
 // Coding Challenge #3
-
 /*
-Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meters)
 
 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
@@ -14,3 +13,42 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 
 GOOD LUCK 😀
 */
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+function calcHigherBMI() {
+  if (mark.calcBMI() > john.calcBMI()) {
+    return `${mark.fullName}'s BMI is ${mark.calcBMI()} which is higher than ${
+      john.fullName
+    }'s which is ${john.calcBMI()}`;
+  } else if (mark.calcBMI() < john.calcBMI()) {
+    return `${john.fullName}'s BMI is ${john.calcBMI()} which is higher than ${
+      mark.fullName
+    }'s which is ${mark.calcBMI()}`;
+  } else {
+    return `The BMI's are the same`;
+  }
+}
+
+// Render the output calling the function that calls the object methods.
+console.log(calcHigherBMI());
