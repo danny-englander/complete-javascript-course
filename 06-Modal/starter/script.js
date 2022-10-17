a6e7be9c819b2287c8881b2c727c568cdfebf237
danny-englander <dannyenglander311@gmail.com>
2022-10-17 13:08:42 -0400

    Finish up modal project with a custom esc key function

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,12 +6,13 @@ const overlay = document.querySelector(".overlay");
 // More than one open buttons, use querySelectorAll.
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-// Close modal function.
+// Close modal custom function.
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
+// Open modal custom function.
 const OpenModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
@@ -26,3 +27,16 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 // Call the custom function to close the modal for these elements.
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// global function for the document
+document.addEventListener("keydown", function (e) {
+  // test what key is pressed.
+  // console.log(e.key);
+
+  // If the escape key is pressed.
+  // If the modal does not contain the hidden class, do not close the modal.
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    // Call the closeModal function when the escape key is pressed.
+    closeModal();
+  }
+});
