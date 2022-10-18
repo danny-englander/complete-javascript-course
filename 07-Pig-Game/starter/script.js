49e5e2ee71844a6cf71ad34bacfc0911e669175e
danny-englander <dannyenglander311@gmail.com>
2022-10-18 08:34:31 -0400

    Dice game, building switch logic

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,14 +2,18 @@
 
 // Define player vars.
 // Player 1
-const player0 = document.querySelector(".player--0");
-const name0 = document.getElementById("name--0");
-const currentScore0 = document.getElementById("current--0");
+
+const playerEL = document.querySelector(".player");
+const playerActiveEL = document.querySelector(".player--active");
+
+const player0EL = document.querySelector(".player--0");
+const name0EL = document.getElementById("name--0");
+const current0EL = document.getElementById("current--0");
 
 // Player 2
-const player1 = document.querySelector(".player--1");
-const name1 = document.getElementById("name--1");
-const currentScore1 = document.getElementById("current--1");
+const player1EL = document.querySelector(".player--1");
+const name1EL = document.getElementById("name--1");
+const current1EL = document.getElementById("current--1");
 
 // Define buttons.
 const btnNew = document.querySelector(".btn--new");
@@ -23,8 +27,12 @@ const diceEL = document.querySelector(".dice");
 const score0EL = document.getElementById("score--0");
 const score1EL = document.getElementById("score--1");
 
+// Starting scores array for each player, 0 points for each side.
+const scores = [0, 0];
 // Define the current score
 let currentScore = 0;
+// Active player starts at the first player, player 1 (0)
+let activePlayer = 0;
 
 // Set the initial score text content to 0.
 // Starting states.
@@ -43,6 +51,15 @@ btnRoll.addEventListener("click", function () {
   // Display the dice roll.
   diceEL.classList.remove("hidden");
 
+  // if (
+  //   playerEL.classList.contains("player--active") &&
+  //   playerEL.classList.contains("player--0")
+  // ) {
+  //   console.log("the current player is player 1");
+  // } else {
+  //   console.log("the current player is player 2");
+  // }
+
   // Set the dice number image displayed on click to match the number rolled.
   diceEL.src = `dice-${dice}.png`;
 
@@ -53,9 +70,22 @@ btnRoll.addEventListener("click", function () {
     currentScore += dice;
     console.log("curr score", currentScore);
     // Update the player score as you roll the dice.
-    currentScore0.textContent = currentScore;
+    // TODO: FIXME: this will have to be changed later for each player.
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // Switch to the next player when a 1 is rolled.
   } else {
-    console.log("I rolled a 1");
+    // Switch to the new active player with a ternary operator.
+    // if the active player is 0, then make the current player 1 and vice-versa.
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    console.log("The active player is now player", activePlayer);
+    // Reset the current score to zero when the player switches after a 1 is rolled..
+    currentScore = 0;
+
+    document
+      .getElementById(`player--${activePlayer}`)
+      .classList.add("player--active");
   }
 
   console.log(dice);
