229fe2c8ada02efdee77d1f76713334a7a2a80e1
danny-englander <dannyenglander311@gmail.com>
2022-10-18 15:28:13 -0400

    Finish scope examples.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,8 +7,8 @@ function calcAge(birthYear) {
   console.log(firstName);
 
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
-    console.log(output);
+    let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    console.log("orig output:", output);
 
     // Block scope
     if (birthYear >= 1981 && birthYear <= 1996) {
@@ -16,13 +16,30 @@ function calcAge(birthYear) {
       const str = `Oh and you are a millenial ${firstName}`;
       console.log(str);
     } else {
+      // Local scope supersedes global scope here.
+      // So JS looks in the local scope first and will
+      // not perform a global scope lookup.
+      const firstName = "Johnny";
+      // Not a millenial.
       var millenial = false;
       const str = `Oh and you are not a millenial ${firstName}`;
       console.log(str);
+
+      // This will not be available outside this scope.
+      function add(a, b) {
+        return a + b;
+      }
+      // This works here as it is immediately outside its scoped function.
+      console.log("2 + 3 =", add(2, 3));
+      output = "new Output!!";
     }
+    console.log(output);
     // Old style ES5 var is available outside of the block scope above.
     // var variables do not care about blocks at all.
     console.log(millenial);
+
+    // This will only work here if strict mode is off which we do not want to do. ^^^
+    // console.log("2 + 3 =", add(2, 3));
   }
 
   printAge();
